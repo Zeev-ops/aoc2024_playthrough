480d3c72043a7e13465d5d7bb8a93623d2df6a3d
Zeev-ops <116187472+Zeev-ops@users.noreply.github.com>
2024-12-02 11:58:36 +0100

    add solution for problem 2

---
+++ day02/solution2.py
@@ -0,0 +1,20 @@
+def checkConditions(line):
+  ascending = all(line[i] <= line[i+1] for i in range(len(line)-1))
+  descending = all(line[i] >= line[i+1] for i in range(len(line)-1))
+  diff = all(1 <= abs(line[i] - line[i+1]) <= 3 for i in range(len(line)-1))
+  return (ascending or descending) and diff
+
+def checkWithRemoval(line):
+  if checkConditions(line):
+    return True
+  for i in range(len(line)):
+    if checkConditions(line[:i] + line[i+1:]):
+      return True
+  return False
+
+report = [[int(num) for num in line.strip().split()] for line in open('input.txt', 'r')]
+correct = 0
+for line in report:
+  if checkWithRemoval(line):
+    correct += 1
+print(correct)
