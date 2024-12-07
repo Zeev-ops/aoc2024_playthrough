ce8982499d1a5b016381cb280d87a038aa446444
Zeev-ops <116187472+Zeev-ops@users.noreply.github.com>
2024-12-07 22:22:28 +0100

    make solution 1 smoother

--- day05/solution1.py
+++ day05/solution1.py
@@ -6,19 +6,13 @@ result = 0
 for o in order:
   expectedOrder = {}
   for page in o:
-    for page in o:
-      expectedOrder[page] = 0 
+    expectedOrder[page] = 0 
   for rule in rules:
     if rule[0] in o and rule[1] in o:
       expectedOrder[rule[0]] += 1
       expectedOrder[rule[1]] -= 1
   expectedOrder = dict(sorted(expectedOrder.items(), key=lambda item: item[1], reverse=True))
-  isOrder = True
-  for i, key in enumerate(expectedOrder.keys()):
-    if o[i] != key:
-      isOrder = False
-      break
-  if isOrder:
-    result += int(o[len(o)//2])
+  keysOrder = list(expectedOrder.keys())
+  if keysOrder == o:
+    result += int(keysOrder[len(keysOrder)//2])
 print(result)
-       
