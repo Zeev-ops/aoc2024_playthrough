9eee121061c88431d0aa9b86674e64dd749a242f
Zeev-ops <116187472+Zeev-ops@users.noreply.github.com>
2024-12-22 23:21:17 +0100

    add solution 2

---
+++ day10/solution2.py
@@ -0,0 +1,22 @@
+def breathFirstSearch(x, y, map):
+  pathwayCount = 0
+  toCheck = [(x, y)]
+
+  while toCheck:
+    x, y = toCheck.pop(0)
+    if map[x][y] == 9:
+      pathwayCount += 1
+      continue
+    for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+      nx, ny = x + dx, y + dy
+      if 0 <= nx < len(map) and 0 <= ny < len(map[0]) and map[nx][ny] - map[x][y] == 1:
+        toCheck.append((nx, ny))
+  return pathwayCount
+        
+map = [[int(i) for i in line.strip()] for line in open('input.txt', 'r')]
+result = 0
+for x in range(len(map)):
+  for y in range(len(map[0])):
+    if map[x][y] == 0:
+      result += breathFirstSearch(x, y, map)
+print(result)
